e6bec575f049040c6d1ec82c230fa9147c029808
rsaleev <rs.aleev@gmail.com>
2023-01-23 20:06:39 +0300

    sign-in form

--- src/components/auth/AuthSignInForm.vue
+++ src/components/auth/AuthSignInForm.vue
@@ -1,64 +1,75 @@
 <template>
-  <v-card
-    width="300px"
-    class="mx-auto px-auto"
-    elevation="12"
-    rounded="10"
-    color="grey-lighten-3"
-    justify-space-around
-  >
-    <v-card-content>
-      <v-form ref="data" v-model="loginFormValid" @submit.prevent="loginUser">
-        <v-text-field
-          v-model="login"
-          :rules="loginRules"
-          label="Логин/Email"
-          required
-        />
-        <v-text-field
-          v-model="password"
-          type="Password"
-          label="Пароль"
-          required
-        />
-        <v-btn
-          :disabled="!loginFormValid"
-          type="submit"
-          color="success"
-          class="mx-5"
-          fab
-        >
-          Войти
-        </v-btn>
-      </v-form>
-    </v-card-content>
-  </v-card>
+  <v-container>
+    <v-card
+      width="300px"
+      class="mx-auto px-auto"
+      elevation="12"
+      rounded="10"
+      color="grey-lighten-3"
+      justify-space-around
+    >
+      <v-card-item>
+        <v-form ref="data" v-model="loginFormValid" @submit.prevent="loginUser">
+          <v-text-field
+            v-model="login"
+            :rules="loginRules"
+            label="Логин/Email"
+            required
+          />
+          <v-text-field
+            v-model="password"
+            type="Password"
+            label="Пароль"
+            required
+          />
+          <v-btn
+            :disabled="!loginFormValid"
+            type="submit"
+            color="success"
+            class="mx-5"
+            fab
+          >
+            Войти
+          </v-btn>
+        </v-form>
+      </v-card-item>
+    </v-card>
+  </v-container>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 
 const store = useStore()
 const router = useRouter()
-// vars
-let data = new FormData()
-let login = ref(store.getters['login/LOGIN_GET'])
-let password = ref(store.getters['login/PASSWORD_GET'])
-let loginFormValid = ref(false)
 
-let loggedIn = computed(() => store.getters['auth/LOGGED_IN_GET'])
+const data = new FormData()
+const loginFormValid = ref(false)
+const login = ref(store.getters['login/LOGIN_GET'])
+const password = ref(store.getters['login/PASSWORD_GET'])
+const loggedIn = computed(() => store.getters['auth/LOGGED_IN_GET'])
 
 function loginUser() {
-  data.append('username', login.value)
-  data.append('password', password.value)
-  store.dispatch('auth/LOGIN_USER', data)
-  if (loggedIn.value == true) {
-    store.dispatch('login/CLEAR_CREDENTIALS')
-    router.push('/')
+  if ((login.value !== null) & (password.value !== null)) {
+    data.append('username', login.value)
+    data.append('password', password.value)
+    store.dispatch('auth/LOGIN_USER', data)
+    if (loggedIn.value == true) {
+      store.dispatch('login/CLEAR_CREDENTIALS')
+      router.push('/objects')
+    }
   }
 }
+
+onMounted(() =>
+  watch([login, password], () => {
+    if (login.value !== '' && password.value !== '') {
+      loginFormValid.value = true
+    }
+  })
+)
 </script>
 
 <style scoped>
