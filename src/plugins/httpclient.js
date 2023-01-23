abee15e4d1e2fddc7a48f632b7341042188d13c4
rsaleev <rs.aleev@gmail.com>
2023-01-23 20:07:47 +0300

    fixed getter prop

--- src/plugins/httpclient.js
+++ src/plugins/httpclient.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
-// import store from '@/store'
+import store from '@/store'
 
 const httpClient = axios.create({
   baseURL: process.env.VUE_APP_BACKEND_URL,
   withCredentials: false,
   timeout: 30000,
-  // headers: { Authorization: `Bearer ${store.getters['auth/tokenGet']}` }
-  headers: { 'Access-Control-Allow-Origin': 'http://localhost' }
+  headers: {
+    Authorization: `Bearer ${store.getters['auth/TOKEN_GET']}`,
+    'Access-Control-Allow-Origin': 'http://localhost'
+  }
 })
 
 export default httpClient
